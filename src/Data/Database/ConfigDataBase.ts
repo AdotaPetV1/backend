e323d21db00557c94e5ff55f1f2e1ccca7ac7beb
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2021-05-13 21:51:20 -0300

    database

--- src/Data/Database/ConfigDataBase.ts
+++ src/Data/Database/ConfigDataBase.ts
@@ -2,12 +2,11 @@ export const knex = require('knex')({
     client: 'mssql',
     connection:{
         host: 'adotapet.database.windows.net',
-        user: 'lucas.vilas',
-        password: 'Luc@sVil@s1407',
+        user: '',
+        password: '',
         database: 'adota_pet',"options": {
             "enableArithAbort": true,
             "encrypt": true
         }
     }
 });
-//Teste
