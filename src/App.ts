4b96051ec728f48874b409c5d84cb8f704a011e1
Lucas Vilas Boas <lucasvilasboaslage@gmail.com>
2021-05-24 20:03:10 -0300

    Atualizando a porta

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,6 @@
 const application = require('./Server');
 
-application.listen(8080, () =>{
+application.listen(process.env.PORT || 8080, () =>{
     console.log("Aplicação rodando em : http://localhost:8080/api/docs")
 });
 
--- src/Server.ts
+++ src/Server.ts
@@ -22,6 +22,7 @@ application.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+
 application.use(cors());
 application.use(bodyParser.json());
 application.use(bodyParser.urlencoded({ extended: false }));
