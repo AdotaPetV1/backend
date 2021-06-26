af962c4b70b0bea52a678b2ceb2cbb73abb8ef51
Damaris Dwayne da Silva <damarisdwayne@outloo.com>
2021-06-26 17:44:49 -0300

    Swagger adicionado

--- src/Controller/AnimalController.ts
+++ src/Controller/AnimalController.ts
@@ -7,6 +7,21 @@ const multer = require('../Middleware/Utils/FileUpload');
 
 router.get('/animal', async(req : Request ,res : Response)=>{
 
+/* #swagger.tags = ['Animal']
+    #swagger.description = 'Retorna todos os animais'
+    #swagger.summary = 'Exibi todos os animais cadastrados'
+
+    #swagger.responses[500] = {
+        description: 'Ocorreu um erro ao buscar os animais!'
+    }
+    
+    #swagger.parameters['UF'] = {
+          type: 'String'
+          
+      }
+
+    */
+
     const { UF }  = req.body;
     const result = await GetAll(UF);
     
@@ -17,6 +32,29 @@ router.get('/animal', async(req : Request ,res : Response)=>{
 });
 
 router.get('/animal/:ID',  async(req : Request ,res : Response) =>{
+
+    /*
+    #swagger.tags = ['Animal']
+    // #swagger.description = 'Retorna o animal cujo id foi informado.'
+    // #swagger.summary = 'Exibi o animal cujo id foi informado.'
+
+    #swagger.responses[200] = {
+        description: 'Get Successfully '
+    }
+    
+
+    #swagger.responses[500] = {
+        description: 'Favor passar um ID válido!!'
+    }
+    
+    
+    #swagger.parameters['id'] = {
+          type: 'Integer',
+          description: 'Id do animal'
+      }
+
+    */
+
     const { ID } = req.params;
 
     const result = await GetAnimalByID(Number(ID));
@@ -29,6 +67,26 @@ router.get('/animal/:ID',  async(req : Request ,res : Response) =>{
 });
 
 router.post('/animal',ValidateToken, async(req : Request ,res : Response)=>{
+
+    /*
+    #swagger.tags = ['Animal']
+    // #swagger.description = 'Rota que cadastra os animais.
+    
+    // #swagger.summary = 'Cadastra os animais.'
+
+    #swagger.responses[200] = {
+        description: 'Post Successfully '
+    }
+    
+    
+    #swagger.responses[400] = {
+        description: 'Ocorreu um erro ao cadastrar os animais!!'
+    }
+    
+    
+
+    */
+
     const result = await PostAnimal(req.body);
 
     res.status(result.statusCode).send({
@@ -38,6 +96,24 @@ router.post('/animal',ValidateToken, async(req : Request ,res : Response)=>{
 });
 
 router.put('/animal', ValidateToken, async(req : Request ,res : Response) =>{
+
+    /*
+    #swagger.tags = ['Animal']
+    // #swagger.description = 'Rota que atualiza os dados dos animais.'
+    // #swagger.summary = 'Atualiza os dados dos animais.'
+
+    #swagger.responses[200] = {
+        description: 'Animal atualizado com sucesso!'
+    }
+    
+
+    #swagger.responses[400] = {
+        description: 'Ocorreu um erro ao atualizar o animal!'
+    }
+    
+   
+    */
+
     const result = await UpdateAnimal(req.body);
 
     res.status(result.statusCode).send({
@@ -47,6 +123,26 @@ router.put('/animal', ValidateToken, async(req : Request ,res : Response) =>{
 });
 
 router.delete('/animal/:ID', ValidateToken, async(req : Request ,res : Response)=>{
+
+    /*
+    #swagger.tags = ['Animal']
+    // #swagger.description = 'Rota que deleta o animal cujo o ID foi informado.'
+    // #swagger.summary = 'Deleta o animal cujo o ID foi informado.'
+    
+
+    #swagger.responses[200] = {
+        description: 'Animal excluído com sucesso!'
+    }
+    
+    #swagger.responses[400] = {
+        description: 'Favor passar um ID válido!'
+    }
+
+    #swagger.responses[500] = {
+        description: 'Ocorreu um erro ao deletar o animal!'
+    }
+   
+    */
 
     const { ID } = req.params;
 
@@ -58,6 +154,28 @@ router.delete('/animal/:ID', ValidateToken, async(req : Request ,res : Response)
 });
 
 router.post('/animal/upload', ValidateToken, multer.single('image'), async(req: any, res: Response)=>{
+
+    /*
+    #swagger.tags = ['Animal']
+    // #swagger.description = 'Rota que possibilita o upload de imagens dos animais.'
+    // #swagger.summary = 'Envio de imagens dos animais.'
+
+    #swagger.responses[200] = {
+        description: 'Arquivo enviado com sucesso!'
+    }
+   
+
+    #swagger.responses[400] = {
+        description: 'Houve erro no upload!'
+    }
+
+    #swagger.parameters['file'] = {
+          type: 'String',
+          description: 'Arquivo que o usuário deseja enviar.'
+      }
+   
+    */
+
     console.log("OI")
     console.log(req.file)
     console.log(req.form)
--- src/Controller/OngController.ts
+++ src/Controller/OngController.ts
@@ -5,6 +5,24 @@ import { Delete, GetAll, GetById, PostOng,PutOrg } from '../Services/OngService'
 const router = express.Router();
 
 router.post('/ong', async (req: Request, res: Response) => {
+
+    /*
+    #swagger.tags = ['Ong']
+    // #swagger.description = 'Rota que realiza o cadastro de uma Ong.' 
+        #swagger.summary = 'Realiza o cadastro de uma Ong.'
+
+        #swagger.responses[200] = {
+        description: 'Ong Cadastrada com Sucesso!'
+    }
+    
+
+    #swagger.responses[400] = {
+        description: 'Erro ao cadastrar a Ong!'
+    }
+    
+   
+    */
+
     const response = await PostOng(req.body);
 
     res.status(response.statusCode).send({
@@ -13,6 +31,25 @@ router.post('/ong', async (req: Request, res: Response) => {
 });
 
 router.put('/ong', ValidateToken, async(req : Request, res: Response) => {
+
+    /*
+    #swagger.tags = ['Ong']
+    // #swagger.description = 'Rota que atualiza os dados de uma Ong.'
+    #swagger.summary = 'Atualiza os dados de uma Ong.'
+    
+
+    #swagger.responses[200] = {
+        description: 'Ong atualizada com Sucesso!'
+    }
+    
+
+    #swagger.responses[400] = {
+        description: 'Erro ao atualizar a Ong!'
+    }
+    
+   
+    */
+
     const response = await PutOrg(req.body);
 
     res.status(response.statusCode).send({
@@ -21,6 +58,26 @@ router.put('/ong', ValidateToken, async(req : Request, res: Response) => {
 });
 
 router.get('/ong', async(req: Request, res: Response) =>{
+
+    /*
+    #swagger.tags = ['Ong']
+    // #swagger.description = 'Rota que mostra todas as Ongs cadastradas.'
+        #swagger.summary = 'Exibi todas as Ongs cadastradas.'
+        
+
+    #swagger.responses[200] = {
+        description: 'Get Successfully'
+    }
+    
+
+    #swagger.responses[500] = {
+        description: 'Ocorreu um erro ao tentar buscar as ongs!'
+    }
+    
+   
+    */
+
+
     const response = await GetAll();
     
     res.status(response.statusCode).send({
@@ -29,6 +86,29 @@ router.get('/ong', async(req: Request, res: Response) =>{
 });
 
 router.get('/ong/:ID', async(req: Request, res: Response) =>{
+
+    /*
+    #swagger.tags = ['Ong']
+    // #swagger.description = 'Rota que retorna uma Ong pelo Id informado.'
+       #swagger.summary = 'Exibi a Ong cujo Id foi informado.'
+       
+
+    #swagger.responses[200] = {
+        description: 'Get Successfully'
+    }
+    
+
+    #swagger.responses[500] = {
+        description: 'Ocorreu um erro ao tentar buscar a ong pelo ID!'
+    }
+
+    #swagger.parameters['id'] = {
+          type: 'Integer',
+          description: 'Id da Ong'
+      }
+    
+   
+    */
     
     const { ID } = req.params;
 
@@ -40,6 +120,30 @@ router.get('/ong/:ID', async(req: Request, res: Response) =>{
 });
 
 router.delete('/ong/:ID', ValidateToken, async(req: Request, res: Response) =>{
+
+    /*
+    #swagger.tags = ['Ong']
+    // #swagger.description = 'Rota que deleta a Ong cujo o ID foi informado.'
+    // #swagger.summary = 'Deleta a Ong cujo o ID foi informado.'
+    
+
+    #swagger.responses[200] = {
+        description: 'ONG excluída com sucesso!'
+    }
+    
+
+    #swagger.responses[500] = {
+        description: 'Ocorreu um erro ao tentar deletar a ONG!'
+    }
+
+    #swagger.parameters['id'] = {
+          type: 'Integer',
+          description: 'Id da Ong'
+      }
+    
+   
+    */
+
     const { ID } = req.params;
 
     const response = await Delete(Number(ID));
